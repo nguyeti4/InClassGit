7a59ef2a7ebbd0c706e6b13a365409ad6fae165b
nguyeti4 <nguyeti4@oregonstate.edu>
2021-04-15 16:51:29 -0700

    list of operations

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
 def calc(a,b):
-    sum = a + b
-    print(sum)
+    summ = a + b
+    #print(sum)
+    difference = a-b
+    multiply = a*b
+    divide = a/b
+    
+    array = [summ,difference,multiply,divide]
+ 
+ 
