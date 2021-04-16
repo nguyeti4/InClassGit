376e6b5211103e242744fb23098739a450114448
nguyeti4 <nguyeti4@oregonstate.edu>
2021-04-15 23:28:55 -0700

    changed password to string datatype

--- password.py
+++ password.py
@@ -2,11 +2,11 @@ import random
 
 def gen_pass():
     num = int(input("Enter a number: "))
-    password = []
+    password = " "
     for i in range(num):
         r = random.randint(65,90)
         letter = chr(r)
-        pass.append(letter)
+        password = password + letter
     print(password)
 
 gen_pass()
