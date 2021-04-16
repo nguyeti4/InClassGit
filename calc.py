af980502adb012c86b4f2bff61822bafb2b0d197
nguyeti4 <nguyeti4@oregonstate.edu>
2021-04-15 17:12:16 -0700

    difference

--- calc.py
+++ calc.py
@@ -1,12 +1,10 @@
 def calc(a,b):
     summ = a + b
-    #print(sum)
     difference = a-b
     multiply = a*b
     divide = a/b
     
     array = [summ,difference,multiply,divide]
-    
     
     total = 0
     for x in array:
