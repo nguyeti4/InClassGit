bda721290ad4cd0ace5161acc47f886ae84ecb18
nguyeti4 <nguyeti4@oregonstate.edu>
2021-04-15 23:01:07 -0700

    made a forloop

--- divisor.py
+++ divisor.py
@@ -1,9 +1,10 @@
 def list_divisors(num):
     #empty list
     list = []
+    #iterate through numbers 1->num
     for i in range(1,num+1):
         if num % i == 0:
             list.append(i)
-    print(list)
+    #print(list)
 
 list_divisors(42)
